73d04d2fa23e3f4bdc81372397b4520a8ab16867
QuadroMan1 <quadroman95@gmail.com>
2015-11-02 07:12:07 -0500

    Setup and position Master Volume slider, no function yet, just slides and displays current value.

---
+++ Assets/John/Options/ValueManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ValueManager : MonoBehaviour {
+    public Text masterVolumeText;
+    public Slider masterVolumeSlider;
+
+    void Update()
+    {
+        masterVolumeText.text = "" + Mathf.Round(masterVolumeSlider.value * 100);
+    }
+}
